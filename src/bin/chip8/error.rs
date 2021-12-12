75274ab7f843b39150d5b502dcc1af708457f2e8
Jonas4420 <jonas.lejeune4420@gmail.com>
2021-12-12 10:56:48 +0900

    Refactor window errors

--- src/bin/chip8/error.rs
+++ src/bin/chip8/error.rs
@@ -6,10 +6,15 @@ pub enum Error {
     InvalidFramerate(u32),
     InvalidScale(usize, u8),
     InvalidScreenSize((usize, usize)),
-    SdlIntegerOrError(sdl2::IntegerOrSdlError),
-    SdlString(String),
-    SdlWindowBuild(sdl2::video::WindowBuildError),
+    Sdl(SdlError),
     UnknownMapping(char),
+}
+
+#[derive(Debug)]
+pub enum SdlError {
+    IntegerOrError(sdl2::IntegerOrSdlError),
+    String(String),
+    WindowBuildError(sdl2::video::WindowBuildError),
 }
 
 impl fmt::Display for Error {
@@ -18,9 +23,11 @@ impl fmt::Display for Error {
             Self::InvalidFramerate(fps) => write!(f, "invalid framerate of {} fps", fps),
             Self::InvalidScale(x, scale) => write!(f, "cannot scale size of {} by {} times", x, scale),
             Self::InvalidScreenSize(dim) => write!(f, "screen size of {}x{} cannot be created", dim.0, dim.1),
-            Self::SdlString(msg) => write!(f, "sdl: {}", msg),
-            Self::SdlIntegerOrError(err) => write!(f, "sdl: {}", err),
-            Self::SdlWindowBuild(err) => write!(f, "sdl: {}", err),
+            Self::Sdl(err) => match err {
+                SdlError::IntegerOrError(err) => err.fmt(f),
+                SdlError::String(err) => err.fmt(f),
+                SdlError::WindowBuildError(err) => err.fmt(f),
+            },
             Self::UnknownMapping(c) => write!(f, "char '{}' has no associated keyboard mapping", c),
         }
     }
@@ -28,19 +35,19 @@ impl fmt::Display for Error {
 
 impl From<sdl2::IntegerOrSdlError> for Error {
     fn from(err: sdl2::IntegerOrSdlError) -> Self {
-        Self::SdlIntegerOrError(err)
+        Self::Sdl(SdlError::IntegerOrError(err))
     }
 }
 
 impl From<String> for Error {
     fn from(err: String) -> Self {
-        Self::SdlString(err)
+        Self::Sdl(SdlError::String(err))
     }
 }
 
 impl From<sdl2::video::WindowBuildError> for Error {
     fn from(err: sdl2::video::WindowBuildError) -> Self {
-        Self::SdlWindowBuild(err)
+        Self::Sdl(SdlError::WindowBuildError(err))
     }
 }
 
